afec3cb8336a05df4416148fdd9be4e9b584a414
Gratcy <gratcypalma@gmail.com>
2018-08-06 22:04:22 +0700

    remove unused script

--- application/views/footer.php
+++ application/views/footer.php
@@ -20,20 +20,10 @@
     <script src="<?php echo site_url('assets'); ?>/js/lib/sticky-kit-master/dist/sticky-kit.min.js"></script>
 
 
-    <script src="<?php echo site_url('assets'); ?>/js/lib/datamap/d3.min.js"></script>
-    <script src="<?php echo site_url('assets'); ?>/js/lib/datamap/topojson.js"></script>
-    <script src="<?php echo site_url('assets'); ?>/js/lib/datamap/datamaps.world.min.js"></script>
-    <script src="<?php echo site_url('assets'); ?>/js/lib/datamap/datamap-init.js"></script>
-
-    <script src="<?php echo site_url('assets'); ?>/js/lib/weather/jquery.simpleWeather.min.js"></script>
-    <script src="<?php echo site_url('assets'); ?>/js/lib/weather/weather-init.js"></script>
     <script src="<?php echo site_url('assets'); ?>/js/lib/owl-carousel/owl.carousel.min.js"></script>
     <script src="<?php echo site_url('assets'); ?>/js/lib/owl-carousel/owl.carousel-init.js"></script>
 
 
-    <script src="<?php echo site_url('assets'); ?>/js/lib/chartist/chartist.min.js"></script>
-    <script src="<?php echo site_url('assets'); ?>/js/lib/chartist/chartist-plugin-tooltip.min.js"></script>
-    <script src="<?php echo site_url('assets'); ?>/js/lib/chartist/chartist-init.js"></script>
     <!--Custom JavaScript -->
     <script src="<?php echo site_url('assets'); ?>/js/custom.min.js"></script>
     <script src="<?php echo site_url('assets'); ?>/js/lib/datatables/datatables.min.js"></script>
